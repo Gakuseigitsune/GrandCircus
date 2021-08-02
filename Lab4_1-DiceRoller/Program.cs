bf8bf3ef8433d73125a284fd2a844ec703fc461a
Jose Alvarez <josealvarez@quickenloans.com>
2021-08-02 10:41:01 -0400

    initial empty upload for lab 4 files

---
+++ Lab4_1-DiceRoller/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab4_1_DiceRoller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
---
+++ Lab4_2-MovieList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab4_2_MovieList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
