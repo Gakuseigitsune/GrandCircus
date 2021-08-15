85a0667aa0bde37d7e0ccf8b268914943b5b0816
Jose Alvarez <josealvarez@quickenloans.com>
2021-08-15 17:25:01 -0400

    Initial upload for lab 5_2

---
+++ Lab5_2-CarLot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab5_2_CarLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
