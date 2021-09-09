48a21cfeb23b3182ddc5799a1408b7c4051507b7
Jose Alvarez <josealvarez@quickenloans.com>
2021-09-09 17:34:11 -0400

    Lab 11.1

---
+++ Lab11_1-PersonalWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab11_1_PersonalWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
